430633aa8e53f8cd4cb8e070a46c5b0236426768
null <nuuullnull@gmail.com>
2018-06-19 21:29:42 +0900

    Boidsの動きを調整

--- Assets/Boids/Scripts/BoidsManager.cs
+++ Assets/Boids/Scripts/BoidsManager.cs
@@ -7,26 +7,26 @@ public class BoidsManager : MonoBehaviour {
 	//Boidの数
 	public int NumBoids = 100;
 
+	//Boidの数
 	public GameObject boid;
 
 	public GameObject centerBoid;
 
-	public GameObject bossBoid;
-
 	public GameObject[] boids;
 
-	public float Distance = 10f;
+	public float groupingDistance = 50f;
+	public float SeparationDistance = 10f;
 
-	public float Radius = 10f;
+	public float AreaRadius = 50f;
 
 	void CreateBoidsObject() {
 		for (int i = 0; i < NumBoids; i++) {
 			boids[i] = Instantiate(
 				boid,
 				new Vector3(
-					Random.Range(-100f, 100f),
-					Random.Range(-100f, 100f),
-					Random.Range(-100f, 100f)
+					Random.Range(-50f, 50f),
+					Random.Range(-50f, 50f),
+					Random.Range(-50f, 50f)
 				),
 				Quaternion.identity
 			);
@@ -43,14 +43,38 @@ public class BoidsManager : MonoBehaviour {
 	//結合
 	void Cohesion (int index) {
 		Vector3 center = Vector3.zero;
+		var groupBoids = new List<GameObject>();
+		groupBoids.Add(boids[index]);
+
+		//中心を求める
 		for (int i = 0; i < NumBoids; i++) {
 			if(i != index) {
 				float dist = Vector3.Distance(boids[i].transform.position, boids[index].transform.position);
-				center += boids[i].transform.position;
+				if(dist < groupingDistance) {
+					groupBoids.Add(boids[i]);
+					center += boids[i].transform.position;
+				}
 			}
 		}
 		center /= NumBoids - 1;
-		boids[index].GetComponent<Rigidbody>().velocity += (center - boids[index].transform.position) * 0.1f;
+
+		//中心から一番遠いBoidを求める
+		GameObject leaderBoids = boids[index];
+		float far = 0;
+		if(groupBoids.Count <= 1) {
+			groupBoids[0].GetComponent<Renderer>().material.color = new Color(1f, 1f, 1f,1f);
+		}else{
+			for (int i = 0; i < groupBoids.Count; i++) {
+				groupBoids[i].GetComponent<Renderer>().material.color = new Color(1f - groupBoids.Count * 0.1f, 1f - groupBoids.Count * 0.05f, 1f,1f);
+				float dist = Vector3.Distance(center, groupBoids[i].transform.position);
+				if(far < dist) {
+					leaderBoids = groupBoids[i];
+					far = dist;
+				}
+			}
+		}
+
+		boids[index].GetComponent<Rigidbody>().velocity += (leaderBoids.transform.position - boids[index].transform.position).normalized * 2;
 	}
 
 	//分離
@@ -61,7 +85,7 @@ public class BoidsManager : MonoBehaviour {
 					boids[i].transform.position,
 					boids[index].transform.position
 				);
-				if(dist < Distance) {
+				if(dist < SeparationDistance) {
 					boids[index].GetComponent<Rigidbody>().velocity -= (boids[i].transform.position - boids[index].transform.position) * 0.8f;
 				}
 			}
@@ -73,7 +97,6 @@ public class BoidsManager : MonoBehaviour {
 		Vector3 averageVelocity = Vector3.zero;
 		for (int i = 0; i < NumBoids; i++) {
 			if(i != index) {
-				float dist = Vector3.Distance(boids[i].transform.position, boids[index].transform.position);
 				averageVelocity += boids[i].GetComponent<Rigidbody>().velocity;
 			}
 		}
@@ -88,25 +111,19 @@ public class BoidsManager : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		centerBoid.transform.position = Vector3.zero;
-		bossBoid.transform.position = Vector3.zero;
 		boids = new GameObject[NumBoids];
 		CreateBoidsObject();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		bossBoid.transform.position = new Vector3(
-			Mathf.Cos(Time.time * 10f) * Radius,
-			Mathf.Sin(Time.time * 10f) * Radius,
-			Mathf.Cos(Time.time * 10f) * Radius
-		);
 		for (int i = 0; i < NumBoids; i++) {
 			Cohesion(i);
 			Separation(i);
 			Alignment(i);
 			float dist = Vector3.Distance(Vector3.zero, boids[i].transform.position);
-			if(dist > 500) {
-				boids[i].GetComponent<Rigidbody>().velocity = (-boids[i].transform.position) * 0.1f;
+			if(dist > AreaRadius) {
+				boids[i].GetComponent<Rigidbody>().velocity += (-boids[i].transform.position);
 			}
 		}
 	}
